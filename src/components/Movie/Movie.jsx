96e15a78098532a7b0b49c03b33ee05097003b6f
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-01-26 23:45:50 +0330

    Update Movie.jsx

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -4,12 +4,12 @@ const Movie = ({ movie, children, onSelctionId }) => {
       className="flex justify-between hover:bg-slate-700 rounded-lg px-1 "
       onClick={() => onSelctionId(movie.imdbID)}
     >
-      <div className="flex p-3">
+      <div className="flex p-4">
         <div className="mr-4">
           <img className="w-10 h-13" src={movie.poster} alt={movie.title} />
         </div>
         <div className="text-white text-xs flex flex-col ">
-          <span className="pb-2 font-bold">{movie.title}</span>
+          <span className="pb-2 font-normal">{movie.title}</span>
           {children}
         </div>
       </div>
