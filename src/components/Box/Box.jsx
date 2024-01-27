0e31ba13457b6c113732aa88bb44911c61f5cd15
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-01-27 23:11:03 +0330

    Update Box.jsx

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -3,10 +3,10 @@ import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 
 const Box = ({ children, isOpen, onOpen }) => {
   return (
-    <div className="w-72 md:w-80 lg:w-96 mx-auto md:mx-5 bg-slate-900 min-h-screen rounded-lg relative">
+    <div className="w-72 md:w-80 lg:w-96 mx-auto md:mx-5 bg-slate-800 min-h-screen rounded-lg relative">
       <div className="flex justify-end">
         <button
-          className="text-lg absolute top-2 right-2 text-slate-600"
+          className="text-lg absolute top-2 right-2 text-slate-500"
           onClick={onOpen}
         >
           {isOpen ? <FaPlusCircle /> : <FaMinusCircle />}
