98386d03cf9aadd6f286999e5c36cf1e967ae08b
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-01-27 23:15:41 +0330

    Update ErrorMessage.jsx

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,6 +1,6 @@
 const ErrorMessage = ({ message }) => {
   return (
-    <div className="text-white bg-slate-900 rounded-lg p-6 w-72 text-center font-bold">
+    <div className="text-white bg-slate-900 rounded-lg p-6 w-72 text-center font-normal">
       {message}📛
     </div>
   );
