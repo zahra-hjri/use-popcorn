91739fe7a1ea68d0f2d1dcf0cdd5653180cccff4
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-01-29 14:56:31 -1000

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = ({ children }) => {
   return (
-    <nav className="text-white flex bg-cyan-950 rounded-md p-2 sm:p-2 md:p-3 lg:p-4 justify-between mb-3">
+    <nav className="text-white flex bg-cyan-950 rounded-md p-2 sm:p-3 md:p-4 lg:p-4 justify-between mb-3">
       {children}
     </nav>
   );
