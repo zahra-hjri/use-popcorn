d5e3bf0269d1494d68f7ad3e97695801730b82b6
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-09 18:36:39 -1000

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ const Logo = () => {
     <div>
       <span
         role="img"
-        className="font-normal flex text-xs sm:text-base lg:text-xl sm:p-2"
+        className="font-normal flex text-xs sm:text-base lg:text-xl sm:p-3"
       >
         🍿
         <h1 className="text-sm">usePopcorn</h1>
